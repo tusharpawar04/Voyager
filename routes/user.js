7517b3732dcb7f5d0f355d653faa36bd81413ca6
tushar pawar <tusharpawar4801@gmail.com>
2024-10-04 21:10:27 +0530

    Added Profile Section

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,18 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require('passport');
 const { saveRedirectUrl } = require('../middleware.js');
 const ListingController = require("../controllers/user.js");
+const {isLoggedIn,isOwner,validateListing,validateReview} = require("../middleware.js");
+const Listing = require('../models/listing.js');
+
 
 router.get("/signup",ListingController.rendersignupform);
 router.post("/signup", wrapAsync(ListingController.signup));
+
+router.get('/Profile', isLoggedIn, wrapAsync(async (req, res) => {
+    const userId = req.user._id; 
+    const listings = await Listing.find({ owner: userId }); 
+    res.render('listings/Profile', { currentUser: req.user, listings }); 
+}));
 
 router.get("/login", ListingController.renderlogin);
 
